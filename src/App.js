307d97b16f60f8bf968935246f082b6ed6707486
mawilsn <mawilsn1989@gmail.com>
2021-07-20 07:27:15 -0500

    initial upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+
+import SideBar from './components/SideBar/sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <SideBar />
+      <div className="text">
+      <p>paragrapph</p>
+      <p>paragrapph</p>
+      <p>paragrapph</p>
+      <p>paragrapph</p>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      <h1>Header</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import "./SideBar.css"
+
+
+function SideBar(){
+  return(
+    <div className = 'wrapper'>
+      <div className="sidebar">
+        <ul>
+          <li>Hello!</li>
+        </ul>
+      </div>
+    </div>
+
+  )
+}
+
+export default SideBar;
